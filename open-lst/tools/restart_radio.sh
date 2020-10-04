e1d93640cd123ed5ec8599036d092650cad602d8
Brad Denby <bdenby@cmu.edu>
2020-10-04 12:46:55 -0400

    Revert "updated installation tools"
    This reverts commit e61a079b7f2a1bf5fd41bf93c1a7acefeb9dfce5.
    
    Reverts references to file paths on a specific machine

--- open-lst/tools/restart_radio.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash  -ex
-sudo udevadm control --reload-rules
-sudo udevadm trigger
-sudo systemctl daemon-reload
-sudo systemctl enable radio@0 radio@1
-sudo systemctl status radio@0 radio@1
